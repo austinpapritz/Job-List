e184b13eca397941bfefc20e0be0b6f722c15e46
workmanmcr <workmanmcr@gmail.com>
2023-07-18 11:28:30 -0700

    commit for push

--- JobBoard/Controllers/HomeController.cs
+++ JobBoard/Controllers/HomeController.cs
@@ -3,18 +3,23 @@ using Microsoft.AspNetCore.Mvc;
 
   public class HomeController : Controller
   {
-
-    [Route("/")]
+    [Route("/")] // Get
     public ActionResult Index( string name )
     {
       JobOpening newJobOpening = new JobOpening( name );
       return View( newJobOpening );
     }
     
-
-    [Route("/form")]
+    [Route("/openings/new")] // Get
     public ActionResult CreateForm()
     {
       return View();
+    }
+
+    [Route("/openings")] // Post
+    public ActionResult Create(string name)
+    {
+      JobOpening newJobOpening = new JobOpening( name );
+      return View( "Index", newJobOpening );
     }
   }
